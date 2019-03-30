a4e8aee5843f30ae0ad756420ed33aa16f97421a
Mokhles <mokhleshaj@gmail.com>
2019-03-30 18:35:36 +0100

    firstCommit

---
+++ src/Services/Implementation/OfferService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services.Implementation;
+
+import Services.Interface.OfferServiceInterface;
+
+/**
+ *
+ * @author Asus
+ */
+public class OfferService implements OfferServiceInterface{
+    
+    
+}
