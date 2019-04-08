d4f84723b9a38564a54ea7f2a473982c4ee5e2e7
unfloat <olphazarrouk@gmail.com>
2019-04-08 02:31:09 +0200

    imports

--- src/Controllers/Project/ProjectController.java
+++ src/Controllers/Project/ProjectController.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Views.Project;
+package Controllers.Project;
 
-import Views.Bid.*;
 import Tools.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
